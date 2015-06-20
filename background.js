7a902c85ecb5970725ab1577e5cadb363e977e29
Rubén Díaz <outime@gmail.com>
2015-06-20 21:46:48 +0000

    Address bar icon will only show up in TweetDeck web app.

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(null, {"file": "script.js"});
+	if (tab.url.indexOf('https://tweetdeck.twitter.com') != -1) {
+      chrome.pageAction.show(tab.id);
+      chrome.tabs.executeScript(null, {"file": "script.js"});
+    }
 });
