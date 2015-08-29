64500ce3ef4edf5b68a8285d3c8c6ff454af8c2c
Ruben Diaz <outime@gmail.com>
2015-08-29 19:23:55 +0000

    Build system (gulp) & frontend package management (bower)

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(id, info, tab){
-	if (tab.url.indexOf('https://tweetdeck.twitter.com') != -1) {
-      chrome.pageAction.show(tab.id);
-      chrome.tabs.executeScript(null, {"file": "script.js"});
-    }
-});
---
+++ src/scripts/background.js
@@ -0,0 +1,6 @@
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+  if (tab.url.indexOf('https://tweetdeck.twitter.com') != -1) {
+    chrome.pageAction.show(tab.id);
+    chrome.tabs.executeScript(null, {"file": "scripts/script.js"});
+  }
+});
